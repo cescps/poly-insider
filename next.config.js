ea0232e86da6c0613ef0ad8d238159519ee03fd8
Cesc <francesc.pinyol.s@gmail.com>
2025-10-17 02:06:50 +0200

    Remove Permissions-Policy header completely to fix error

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()'
-          }
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
